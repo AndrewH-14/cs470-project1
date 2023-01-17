54b60ac377d58c860b95125f614beead27ac6031
Andrew Hankins <67705231+AndrewH-14@users.noreply.github.com>
2023-01-16 23:45:14 -0600

    Created mergesort animation

--- mergesort.py
+++ mergesort.py
@@ -1,13 +1,42 @@
 import turtle
+import time
 
-data_string = input()
-data = data_string.split(',')
-data = [int(x) for x in data]
+# Create a global data array so that all of the functions can access it
+data    = []
+turtles = []
+
+# Variables used to customize the screen
+screen    = turtle.Screen()
+writer    = turtle.Turtle()
+border    = 10
+bar_width = 10
 
 def Merge(lower_idx, middle_idx, upper_idx):
     sorted_data     = []
     left_iter       = lower_idx
     right_iter      = middle_idx + 1
+
+    description = turtle.Turtle()
+    description.hideturtle()
+    description.penup()
+    description.goto((bar_width * len(data) + border) / 2, (max(data) + border) / 1.2)
+    description.pendown()
+    description.color("gray")
+    description.write("Dividing into sublists...", False, align="center", font=('Courier', 20, 'bold'))
+    description.penup()
+
+    # Indicate the two sections we are merging by their color
+    for idx in range(lower_idx, middle_idx + 1):
+        turtles[idx].clear()
+        DrawBar(turtles[idx], bar_width * idx, 0, data[idx], "crimson")
+        screen.update()
+        time.sleep(0.2)
+    for idx in range(middle_idx + 1, upper_idx + 1):
+        turtles[idx].clear()
+        DrawBar(turtles[idx], bar_width * idx, 0, data[idx], "white")
+        screen.update()
+        time.sleep(0.2)
+    time.sleep(1)
 
     # Compare elements from either side of the array and add the smaller one to the
     # sorted array
@@ -32,6 +61,31 @@ def Merge(lower_idx, middle_idx, upper_idx):
     for idx in range(len(sorted_data)):
         data[lower_idx + idx] = sorted_data[idx]
 
+    # Provide a description letting the user know the two sublists are being merged
+    description.clear()
+    description.goto((bar_width * len(data) + border) / 2, (max(data) + border) / 1.2)
+    description.pendown()
+    description.color("gray")
+    description.write("Merging sublists...", False, align="center", font=('Courier', 20, 'bold'))
+    description.penup()
+
+    # Update the bar graph to show the resulting sorted data
+    for idx in range(lower_idx, upper_idx + 1):
+        turtles[idx].clear()
+        DrawBar(turtles[idx], bar_width * idx, 0, data[idx], "gold")
+        screen.update()
+        time.sleep(0.2)
+    time.sleep(1)
+    for idx in range(lower_idx, upper_idx + 1):
+        turtles[idx].clear()
+        DrawBar(turtles[idx], bar_width * idx, 0, data[idx], "gray")
+    screen.update()
+    time.sleep(1)
+
+    # Clear the description message
+    description.clear()
+
+# Recursive function to sort sections of the array
 def MergeSort(lower_idx, upper_idx):
     if lower_idx >= upper_idx:
         return
@@ -40,5 +94,94 @@ def MergeSort(lower_idx, upper_idx):
     MergeSort(middle_idx + 1, upper_idx)
     Merge(lower_idx, middle_idx, upper_idx)
 
-MergeSort(0, len(data) - 1)
-print(data)
+# Draws a bar assuming the turtle is facing east
+def DrawBar(t, x, y, height, color):
+    t.fillcolor(color)
+    t.setheading(0)
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    t.begin_fill()
+    t.left(90)
+    t.forward(height)
+    t.right(90)
+    t.forward(bar_width / 2)
+    t.write(str(height))
+    t.forward(bar_width / 2)
+    t.right(90)
+    t.forward(height)
+    t.left(90)
+    t.end_fill()
+
+# Set up the screen for creating the animation
+def InitScreen():
+    # Sets the height and width of the turtle window
+    screen.setup(1000, 700)
+    # Turns of the screen's auto update feature
+    screen.tracer(0)
+    # Sets the screens background color
+    screen.bgcolor("light grey")
+    # Sets the coordinates within the window using the bottom left and
+    # upper right coordinates
+    screen.setworldcoordinates(0 - border,
+                               0,
+                               bar_width * len(data) + border,
+                               max(data) + border)
+    # Creates a title for the screen's window
+    screen.title("Merge Sort Algorithm")
+
+def BeginSorting():
+    screen.onkey(None, "space")
+    writer.clear()
+    writer.goto((bar_width * len(data) + border) / 2, (max(data) + border) / 1.1)
+    writer.pendown()
+    writer.write("Sorting...", False, align="center", font=('Courier', 25, 'bold'))
+    writer.penup()
+    # Start the merge sort algorithm
+    MergeSort(0, len(data) - 1)
+    writer.clear()
+    writer.goto((bar_width * len(data) + border) / 2, (max(data) + border) / 1.1)
+    writer.pendown()
+    writer.color("green")
+    writer.write("Complete", False, align="center", font=('Courier', 25, 'bold'))
+    writer.penup()
+
+def main():
+    # Must be included to edit the global variables
+    global data
+    global turtles
+    global writer
+
+    # Store the comma seperated list of integers into the global array
+    data_string = input()
+    data_string_array = data_string.split(',')
+    data = [int(x) for x in data_string_array]
+
+    # Initialize the turtle's screen
+    InitScreen()
+
+    # Create the initial bar graph using the data list
+    for idx in range(len(data)):
+        franklin = turtle.Turtle()
+        franklin.pensize(3)
+        franklin.hideturtle()
+        franklin.speed(0)
+        DrawBar(franklin, bar_width * idx, 0, data[idx], "gray")
+        turtles.append(franklin)
+    screen.update()
+
+    writer.hideturtle()
+    writer.penup()
+    writer.goto((bar_width * len(data) + border) / 2, (max(data) + border) / 1.1)
+    writer.pendown()
+    writer.write("Press space to begin sorting", False, align="center", font=('STHupo', 25, 'bold'))
+    writer.penup()
+
+    screen.onkey(BeginSorting, "space")
+    screen.listen()
+
+    # Keep the turtle screen open until mouse is clicked
+    screen.mainloop()
+
+if __name__ == "__main__":
+    main()
