73288951b30e8d1e27ee7a82ac5f9b083c33e8cf
Andrew Hankins <67705231+AndrewH-14@users.noreply.github.com>
2023-01-17 13:07:58 -0600

    Updated animation.

--- mergesort.py
+++ mergesort.py
@@ -4,6 +4,7 @@ import time
 # Create a global data array so that all of the functions can access it
 data    = []
 turtles = []
+k = 0
 
 # Variables used to customize the screen
 screen    = turtle.Screen()
@@ -18,12 +19,14 @@ def Merge(lower_idx, middle_idx, upper_idx):
 
     description = turtle.Turtle()
     description.hideturtle()
+    description.speed("fastest")
     description.penup()
     description.goto((bar_width * len(data) + border) / 2, (max(data) + border) / 1.2)
     description.pendown()
     description.color("gray")
     description.write("Dividing into sublists...", False, align="center", font=('Courier', 20, 'bold'))
     description.penup()
+    screen.update()
 
     # Indicate the two sections we are merging by their color
     for idx in range(lower_idx, middle_idx + 1):
@@ -68,6 +71,7 @@ def Merge(lower_idx, middle_idx, upper_idx):
     description.color("gray")
     description.write("Merging sublists...", False, align="center", font=('Courier', 20, 'bold'))
     description.penup()
+    screen.update()
 
     # Update the bar graph to show the resulting sorted data
     for idx in range(lower_idx, upper_idx + 1):
@@ -131,14 +135,18 @@ def InitScreen():
     screen.title("Merge Sort Algorithm")
 
 def BeginSorting():
+    # Write the sorting status message to the screen
     screen.onkey(None, "space")
     writer.clear()
     writer.goto((bar_width * len(data) + border) / 2, (max(data) + border) / 1.1)
     writer.pendown()
     writer.write("Sorting...", False, align="center", font=('Courier', 25, 'bold'))
     writer.penup()
+
     # Start the merge sort algorithm
     MergeSort(0, len(data) - 1)
+
+    # Write the completion status message to the screen
     writer.clear()
     writer.goto((bar_width * len(data) + border) / 2, (max(data) + border) / 1.1)
     writer.pendown()
@@ -146,14 +154,35 @@ def BeginSorting():
     writer.write("Complete", False, align="center", font=('Courier', 25, 'bold'))
     writer.penup()
 
+    # Write a description message to the screen
+    description = turtle.Turtle()
+    description.hideturtle()
+    description.speed("fastest")
+    description.penup()
+    description.goto((bar_width * len(data) + border) / 2, (max(data) + border) / 1.2)
+    description.pendown()
+    description.color("gray")
+    description.write("Displaying top " + str(k) + " integers", False, align="center", font=('Courier', 20, 'bold'))
+    description.penup()
+    screen.update()
+
+    # Show the top k integers in the data list
+    for idx in range(len(data) - k, len(data)):
+        turtles[idx].clear()
+        DrawBar(turtles[idx], bar_width * idx, 0, data[idx], "green")
+        screen.update()
+        time.sleep(0.2)
+
 def main():
     # Must be included to edit the global variables
     global data
     global turtles
     global writer
+    global k
 
     # Store the comma seperated list of integers into the global array
     data_string = input()
+    k = int(input())
     data_string_array = data_string.split(',')
     data = [int(x) for x in data_string_array]
 
@@ -165,16 +194,17 @@ def main():
         franklin = turtle.Turtle()
         franklin.pensize(3)
         franklin.hideturtle()
-        franklin.speed(0)
+        franklin.speed("fastest")
         DrawBar(franklin, bar_width * idx, 0, data[idx], "gray")
         turtles.append(franklin)
     screen.update()
 
     writer.hideturtle()
+    writer.speed("fastest")
     writer.penup()
     writer.goto((bar_width * len(data) + border) / 2, (max(data) + border) / 1.1)
     writer.pendown()
-    writer.write("Press space to begin sorting", False, align="center", font=('STHupo', 25, 'bold'))
+    writer.write("Press space to begin sorting", False, align="center", font=('Courier', 25, 'bold'))
     writer.penup()
 
     screen.onkey(BeginSorting, "space")
